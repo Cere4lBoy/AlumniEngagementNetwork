a39764df81f86fc9224f89f8afc35f07f1c65a43
Narmatha-05 <167971654+Narmatha-05@users.noreply.github.com>
2026-02-03 00:14:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask
+from routes.officer_routes import officer_bp
 
 app = Flask(__name__)
 
+# Register officer routes
+app.register_blueprint(officer_bp)
 
-#IMAN'S
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return """
+    <h2>AENS Flask is running ✅</h2>
+    <p>Go to <a href="/officer/dashboard">/officer/dashboard</a></p>
+    """
 
-@app.route("/student")
-def student():
-    return render_template("/Dashboard/student.html")
-
-@app.route("/alumni")
-def alumni():
-    return render_template("/Dashboard/alumni.html")
-
-@app.route("/officer")
-def officer():
-    return render_template("/Dashboard/officer.html")
-
-@app.route("/admin")
-def admin():
-    return render_template("/Dashboard/admin.html")
-
-
-
-#nono touch
 if __name__ == "__main__":
     app.run(debug=True)
